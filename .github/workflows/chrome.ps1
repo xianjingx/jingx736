7645e9463c47be80dd7bd1764a2e4ef4de2ca5da
xianjingx <52995011+xianjingx@users.noreply.github.com>
2021-09-02 21:57:42 +0700

    Create chrome.ps1

---
+++ .github/workflows/chrome.ps1
@@ -0,0 +1,1 @@
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AX4XfWgtKL_G3qOOLTUNMKznUXERVvn3CvCtFCsw4XMZFoYng0VXoiZSbcrGphuq9FrzIA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME
